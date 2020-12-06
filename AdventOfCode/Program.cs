1bc25e0faccfa3f69b2c283d6378dd032b46b9b5
Adrian Jezewski <adrian.jezewski@avanade.com>
2020-12-06 18:04:08 +0100

    updated Main()

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,19 +1,38 @@
-﻿namespace AdventOfCode
+﻿using AdventOfCode2020;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("* * * DAY1 * * *");
-            var day1 = new Day1();
-            day1.Get2020();
-            day1.Get2020from3numbers();
 
-            System.Console.WriteLine("* * * DAY2 * * *");
 
-            var day2 = new Day2();
-            day2.CountPasswords();
-            day2.ValidatePlaces();
+            //Console.WriteLine("* * * DAY 1 * * *");
+            //var day1 = new Day1();
+            //day1.Get2020();
+            //day1.Get2020from3numbers();
+
+            //Console.WriteLine("* * * DAY 2 * * *");
+            //var day2 = new Day2();
+            //day2.CountPasswords();
+            //day2.ValidatePlaces();
+
+            //Console.WriteLine("* * * DAY 3 * * *");
+            //var day3 = new Day3();
+            //day3.CountTree(3,1);
+            //day3.ThroughSlopes();
+
+            //Console.WriteLine("* * * DAY 3 * * *");
+            var day4 = new Day4();
+
+            day4.ValidPassports();
+
+
+
         }
     }
 }
