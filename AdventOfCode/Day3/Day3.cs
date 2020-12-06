1ba7912ad096614529a12b2f08afb0a2a11c9e1b
Adrian Jezewski <adrian.jezewski@avanade.com>
2020-12-06 18:02:50 +0100

    day3 & day4 part1

---
+++ AdventOfCode/Day3/Day3.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    public class Day3
+    {
+
+        char[,] _array;
+
+        public Day3()
+        {
+            _array = Build2dArray();
+        }
+
+        public char[,] Build2dArray()
+        {
+
+            string input = File.ReadAllText("Day3/day3.txt");
+            var rows = input.Split(Environment.NewLine);
+
+            var array = new char[rows.Length, rows[0].Length];
+
+            for (int i = 0; i < rows.Length; i++)
+            {
+                for (int j = 0; j < rows[0].Length; j++)
+                {
+                    array[i, j] = rows[i][j];
+                }
+            }
+            return array;
+        }
+
+        public int CountTree(int right, int down)
+        {
+            var (column, row) = (0, 0);
+            var trees = 0;
+
+            while (row < _array.GetLength(0))
+            {
+                if (_array[row, column] == '#')
+                    trees++;
+
+                column += right;
+                row += down;
+
+                if (column > _array.GetLength(1) - 1)
+                {
+                    column -= _array.GetLength(1);
+                }
+            }
+            Console.WriteLine(trees);
+            return trees;
+        }
+
+        public int ThroughSlopes()
+        {
+            var multiply = CountTree(1, 1) 
+                * CountTree(3, 1) 
+                * CountTree(5, 1) 
+                * CountTree(7, 1) 
+                * CountTree(1, 2);
+            Console.WriteLine(multiply);
+            return multiply;
+        }
+    }
+}
---
+++ AdventOfCode/Day4/Day4.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Day4
+    { 
+        public int ValidPassports()
+        {
+            List<string> inputs = File.ReadAllLines("Day4/day4.txt").ToList();
+            List<string> passports = BulidPassportData(inputs);
+
+            var query = passports.Where(p => p.Split(" ").Count() == 8 ||
+                    (p.Split(" ").Count() == 7 && !p.Contains("cid"))).ToList();
+
+            Console.WriteLine("Number of valid passports : " +query.Count);
+            return query.Count;
+        }
+
+        public List<string> BulidPassportData(List<string> inputs)
+        {
+            List<string> passports = new List<string>();
+            IEnumerator<string> iter = inputs.GetEnumerator();
+
+            while (iter.MoveNext())
+            {
+                var pass = "";
+                var item = iter.Current;
+
+                while (!string.IsNullOrWhiteSpace(item))
+                {
+                    pass += " " + item;
+                    if (!iter.MoveNext()) goto end;
+                    item = iter.Current;
+                }                
+            end:
+                passports.Add(pass.TrimStart());
+            }
+
+            Console.WriteLine("Total number of passports : " +passports.Count);
+            return passports;
+        }
+    }
+}
